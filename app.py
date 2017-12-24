61c300c2caa693cd86f7c56b5898932c75ebc075
TakumaUrata <uratatakuma@Takumas-MacBook-Pro.local>
2017-12-24 18:33:59 +0900

    Add if sentence

--- app.py
+++ app.py
@@ -48,9 +48,14 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.type == "message":
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text=event.message.text),
+            ]
+        )
+
 
 
 if __name__ == "__main__":
