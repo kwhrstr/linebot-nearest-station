647c38eda5174da12bebe24cd0525045a7d4b9b9
TakumaUrata <uratatakuma@Takumas-MacBook-Pro.local>
2017-12-27 00:51:34 +0900

    Try

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def handle_location(event):
     # (3)
     actions = [
         MessageImagemapAction(
-            text = root[0][0].text,
+            text = root.tag,
             area = ImagemapArea(
                 x = 0,
                 y = 0,
