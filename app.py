8d20292d83f2d23149598b4a4388d1ea990aabe8
TakumaUrata <uratatakuma@Takumas-MacBook-Pro.local>
2017-12-29 20:38:49 +0900

    try

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def handle_location(event):
     # (3)
     actions = [
         MessageImagemapAction(
-            text = str(nearest_station_name[0].text),
+            text = str(nearest_station_name[1].text),
             area = ImagemapArea(
                 x = 0,
                 y = 0,
