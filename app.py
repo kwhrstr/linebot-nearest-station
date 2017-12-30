29ada275f39077530b7f4ab5ec565d744a8ce2d5
Takuma Urata <takumaurata@TakumanoMacBook-puro.local>
2017-12-30 19:13:22 +0900

    try

--- app.py
+++ app.py
@@ -104,10 +104,10 @@ def handle_location(event):
     near_station_n = len(near_station_list)
 
     # 最寄駅名から座標を取得
-    near_station_geo_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&key={}'.format(urllib.parse.quote_plus(near_station_list[0].text, encoding='utf-8'), 'AIzaSyDap2dQQx8T0SnMuHQ110Pp5mXDvnldXns');
+    near_station_geo_url = 'https://maps.googleapis.com/maps/api/place/textsearch/xml?query={}&key={}'.format(urllib.parse.quote_plus(near_station_list[0].text, encoding='utf-8'), 'AIzaSyDap2dQQx8T0SnMuHQ110Pp5mXDvnldXns');
     near_station_geo_req = urllib.request.Request(near_station_geo_url) #object
     with urllib.request.urlopen(near_station_geo_req) as response:
-        near_station_geo_read = response.read()
+        near_station_geo_XmlData = response.read()
     #near_location_root = ET.fromstring(near_location_XmlData)
     #near_location_lat = near_location_root.findtext(".//lat")
     #near_location_lon = near_location_root.findtext(".//lng")
@@ -121,7 +121,7 @@ def handle_location(event):
     #i = 0
     actions = [
         MessageImagemapAction(
-            text = str(near_station_geo_req),
+            text = str(near_station_geo_XmlData),
             area = ImagemapArea(
                 x = 0,
                 y = 0,
