0b34ad3c1ccc2b81037c26e096ff2c8c342495ff
Takuma Urata <takumaurata@TakumanoMacBook-puro.local>
2017-12-30 19:42:39 +0900

    try

--- app.py
+++ app.py
@@ -113,7 +113,8 @@ def handle_location(event):
     near_station_geo_lon = near_station_geo_root.findtext(".//lng")
 
     #map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom={}&size=520x520&scale=2&maptype=roadmap&key={}'.format(near_station_geo_lat, near_station_geo_lon, zoomlevel, 'AIzaSyCqPyyXKmQ1Ij290Fja_vxmMo78kViDqSw');
-    map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&size=520x520&scale=2&maptype=roadmap&key={}'.format(near_station_geo_lat, near_station_geo_lon, 'AIzaSyCqPyyXKmQ1Ij290Fja_vxmMo78kViDqSw');
+    #map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&size=520x520&scale=2&maptype=roadmap&key={}'.format(near_station_geo_lat, near_station_geo_lon, 'AIzaSyCqPyyXKmQ1Ij290Fja_vxmMo78kViDqSw');
+    map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?size=520x520&scale=2&maptype=roadmap&key={}'.format('AIzaSyCqPyyXKmQ1Ij290Fja_vxmMo78kViDqSw');
     map_image_url += '&markers=color:{}|label:{}|{},{}'.format('blue', '', near_station_geo_lat, near_station_geo_lon)
     map_image_url += '&markers=color:{}|label:{}|{},{}'.format('blue', '', lat, lon)
 
