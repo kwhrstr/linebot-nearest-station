3156c7bda563be0e9778a57f4b5fe73cf6a04ca0
Takuma Urata <takumaurata@TakumanoMacBook-puro.local>
2017-12-30 19:01:42 +0900

    try

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def handle_location(event):
     near_station_geo_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&key={}'.format(near_station_list[0].text, 'AIzaSyDap2dQQx8T0SnMuHQ110Pp5mXDvnldXns');
     near_station_geo_req = urllib.request.Request(near_station_geo_url) #object
     with urllib.request.urlopen(near_station_geo_req) as response:
-        near_station_geo_read = response.read()
+        near_station_geo_read = response.read().encode('utf-8')
     #near_location_root = ET.fromstring(near_location_XmlData)
     #near_location_lat = near_location_root.findtext(".//lat")
     #near_location_lon = near_location_root.findtext(".//lng")
