8b3f8069329a33fe5c627aa9a6447e78f64fa6e5
Takuma Urata <takumaurata@TakumanoMacBook-puro.local>
2017-12-30 20:10:30 +0900

    try

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, LocationMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize
+    MessageEvent, TextMessage, TextSendMessage, LocationMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize, LocationSendMessage
 )
 
 from PIL import Image
